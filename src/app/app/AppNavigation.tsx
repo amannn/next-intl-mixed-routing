81eef241ae8708f49d37c8789c1623c97a6d4c85
Jan Amann <jan@amann.me>
2024-06-07 17:33:08 +0200

    Highlight active nav item

--- src/app/app/AppNavigation.tsx
+++ src/app/app/AppNavigation.tsx
@@ -1,14 +1,14 @@
 import {useTranslations} from 'next-intl';
-import Link from 'next/link';
 import Logout from './Logout';
+import NavLink from './NavLink';
 
 export default function AppNavigation() {
   const t = useTranslations('AppNavigation');
 
   return (
     <nav className="flex gap-6 py-5">
-      <Link href="/app">{t('home')}</Link>
-      <Link href="/app/profile">{t('profile')}</Link>
+      <NavLink href="/app">{t('home')}</NavLink>
+      <NavLink href="/app/profile">{t('profile')}</NavLink>
       <div className="ml-auto">
         <Logout />
       </div>
